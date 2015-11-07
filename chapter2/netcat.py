c11a146795598ff0da063a2cb474e4eb62951457
horie1024 <ryosuke.pic@gmail.com>
2015-11-07 23:54:04 +0900

    Add any files

---
+++ chapter2/netcat.py
@@ -0,0 +1,51 @@
+# vim:fileencoding=utf-8
+
+import sys
+import socket
+import getopt
+import threading
+import subprocess
+
+listen = False
+command = False
+upload = False
+execute = ""
+target = ""
+upload_destination = ""
+port = 0
+
+def usage:
+
+    sys.exit(0)
+
+def main():
+
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    try:
+        opts, args = getopt.getopt(
+                sys.argv[1:],
+                "hle:t:p:cu:",
+                {"help", "listen", "execute=", "target=",
+                 "port", "command", "upload="})
+
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-l", "-listen"):
+            listen = True
+        elif o in ("-e", "-- execute"):
+            execute = a
+
